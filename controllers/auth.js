aac004a861db50423d0d986aa99cd7fd8211cba8
AungHeeinZay <ahz007aunghz@gmail.com>
2025-06-21 11:03:13 +0630

    this commit is not finish

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
 exports.renderLogin=(req,res)=>{
     res.render("auth/login")
 }
@@ -9,4 +11,26 @@ exports.postLogin=(req,res)=>{
 exports.Logout=(req,res)=>{
     req.session.destroy()
     res.redirect("/")
+}
+
+exports.renderRegister=(req,res)=>{
+    res.render("auth/register")
+}
+
+exports.register=async(req,res)=>{
+    const {username,email,password} = req.body
+    try{
+    const user =await User.findOne({email})
+    if(user){
+        return res.redirect("/register")
+    }
+    await User.create({
+        username,
+        email,
+        password:await bcrypt.hash(password,10)
+    })
+    res.redirect("/login")
+    }catch(err){
+        console.log(err);
+    }
 }
--- routers/auth.js
+++ routers/auth.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/auth")
 router.post("/logout",authController.Logout)
 router.get("/login",authController.renderLogin)
 router.post("/login",authController.postLogin)
-
+router.get("/register",authController.renderRegister)
+router.post("/register",authController.register)
 module.exports = router
